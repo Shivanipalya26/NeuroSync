39fbb292c6b5c68bae52660448c9a73c02e3a0f1
Shivani Palya <shivanipalya55@gmail.com>
2025-02-20 22:27:32 +0530

    refactor: Modified footer for mobile view and added Logo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="mt-16 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4 px-6">
+        <div className="mt-16 grid grid-cols-2 gap-8 lg:grid-cols-4 px-6">
           {FooterLinks.map((section, index) => (
             <div key={index} className="text-center sm:text-left">
               <p className="text-lg font-medium">{section.title}</p>
--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -3,6 +3,8 @@ import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { MobileMenu } from "./MobileMenu";
 import { NavSection } from "./NavSection";
 import * as B from "../../styles/buttons/index";
+import { BrainCircuit } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const NavBar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,23 +18,25 @@ const NavBar: React.FC = () => {
 
   return (
     <div className="fixed top-10 inset-x-0 max-w-6xl mx-auto z-50">
-          <nav className="relative rounded-full border border-white bg-black shadow-input flex justify-center space-x-4 px-4 py-3 md:px-8 md:py-3 ">
-            <div className="flex-grow">
-              <span className="text-xl">Logo</span>
-            </div>
-            <div className="hidden md:flex flex-grow justify-center">
-              <NavSection />
-            </div>
-    
-            <div className="hidden md:flex space-x-2">
-              <B.Button variant="secondary" rounded="full">
-                Log In
-              </B.Button>
-              <B.Button rounded="full">Book a Demo</B.Button>
-            </div>
-            <MobileMenu isOpen={isOpen} setIsOpen={setIsOpen} />
-          </nav>
+      <nav className="relative rounded-full border border-white bg-black shadow-input flex justify-center space-x-4 px-4 py-3 md:px-8 md:py-3 ">
+        <div className="flex flex-grow items-center">
+          <Link to="/">
+            <BrainCircuit />
+          </Link>
         </div>
+        <div className="hidden md:flex flex-grow justify-center">
+          <NavSection />
+        </div>
+
+        <div className="hidden md:flex space-x-2">
+          <B.Button variant="secondary" rounded="full">
+            Log In
+          </B.Button>
+          <B.Button rounded="full">Book a Demo</B.Button>
+        </div>
+        <MobileMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+      </nav>
+    </div>
   );
 };
 
